cb2fc701ef4d117e0f3a1436d5905802e3a97d2a
Chen He <airbots@gmail.com>
2016-01-25 15:48:49 -0800

    Change in JTashScheduler is made

--- src/contrib/fairscheduler/src/java/org/apache/hadoop/mapred/AllocationConfigurationException.java
+++ src/contrib/fairscheduler/src/java/org/apache/hadoop/mapred/AllocationConfigurationException.java
@@ -16,7 +16,7 @@
  * limitations under the License.
  */
 
-package org.apache.hadoop.mapred;
+package contrib.fairscheduler.src.java.org.apache.hadoop.mapred;
 
 /**
  * Thrown when the allocation file for {@link PoolManager} is malformed.  
